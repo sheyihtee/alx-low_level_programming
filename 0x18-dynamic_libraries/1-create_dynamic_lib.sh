d9ae1bacef5ef9f1bba9844f1f60e2db96c0a9b3
sheyihtee <sheyihtee@gmail.com>
2023-06-19 14:47:26 -0700

    libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 =======
 gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
 gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
->>>>>>> 2c9e07c3faa4981885713705c1e61b00a5603908
